54ca0d571a0bdee31504bfce8fe0b5c89323ad8e
AouirikaSouhail <aoso1012@student.eafc-uccle.be>
2025-06-01 16:06:06 +0200

    feat: ajout dépendance Lombok
    facilité ajout getter/Setter ...

---
+++ backend/src/main/java/com/tshirtshop/backend/model/Product.java
@@ -0,0 +1,7 @@
+package com.tshirtshop.backend.model;
+
+// Ces annotations viennent de la bibliothèque Lombok.
+// Elles évitent d’écrire tout le code “inutile” comme les getters/setters.
+
+public class Product {
+}
