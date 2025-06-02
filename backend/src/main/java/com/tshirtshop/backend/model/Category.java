a2540250f5b23a50aa20d4f8f5d27a6bb83d8ec2
AouirikaSouhail <aoso1012@student.eafc-uccle.be>
2025-06-02 09:52:14 +0200

    feat: ajout de la relation OneToMany entre Category et Product

--- backend/src/main/java/com/tshirtshop/backend/model/Category.java
+++ backend/src/main/java/com/tshirtshop/backend/model/Category.java
@@ -2,6 +2,8 @@
 package com.tshirtshop.backend.model;
 // On importe des outils de Spring JPA pour faire le lien avec MySQL
 import jakarta.persistence.*;
+import com.tshirtshop.backend.model.Product;
+import java.util.List;
 
 // Cette classe est une entité → elle sera automatiquement convertie en table category dans MySQL
 
@@ -17,6 +19,11 @@ public class Category {
 
     private String name;   // Nom de la catégorie (ex: “T-Shirts Homme”)
     private String imageUrl; // URL de l’image (ex: "https://image.com/tshirt.png")
+
+    //Cette ligne établit une relation entre une catégorie et plusieurs produits.
+    // mappedBy = "category" fait référence à l'attribut  'category' dans la classe Product.
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
 
    // Constructeur
    public Category() {}
@@ -35,4 +42,13 @@ public class Category {
     public void setName(String name){this.name = name;}
     public String getImageUrl(){return imageUrl;}
     public void setImageUrl(String imageUrl){this.imageUrl = imageUrl;}
+    public List<Product> getProducts(){return products;}
+   public void setProducts(List<Product> products){this.products = products;}
 }
+/** Annotation	Explication
+@OneToMany	Une catégorie peut avoir plusieurs produits.
+mappedBy="category"	Dit à Spring que la relation est déjà définie côté Product dans le champ category.
+cascade = CascadeType.ALL	Si tu supprimes une catégorie, tous les produits associés sont supprimés aussi.
+List<Product> products	On crée une liste qui contient tous les produits liés à cette catégorie.
+
+**/
