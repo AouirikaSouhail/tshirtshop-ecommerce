ca0cb178d4f4ad29de61c93b6b59474957555a8c
AouirikaSouhail <aoso1012@student.eafc-uccle.be>
2025-06-04 14:49:24 +0200

    US01 terminée : frontend affiche les catégories depuis Spring boot

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+
+
+@NgModule({
+  declarations: [// tes composants
+    AppComponent,
+    CategoryListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+/**HttpClient est la classe que j’utilise dans mes services pour faire des appels HTTP.
+Mais pour qu’Angular sache comment créer un objet HttpClient, je dois importer HttpClientModule dans app.module.ts.
+Sans ce module, Angular ne connaît pas HttpClient et ne peut pas l’injecter automatiquement. */
